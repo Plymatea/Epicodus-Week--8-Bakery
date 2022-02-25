220add4da55bf34fc0f7c512eb6438a4f34c26c7
plymatea <andy.plymate@gmail.com>
2022-02-25 13:43:16 -0800

    add basic frontend program work.

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -8,10 +8,34 @@ namespace Bakery
   {
     public static void Main()
     {
-      Console.WriteLine("Hello World");
-      Bread newBread = new Bread();
-      Console.WriteLine(newBread.Price);
-      Console.WriteLine(Bread.GetBreadList()[0].Description);
+      Console.WriteLine("Good Morning!! Guten Tag!! ");
+      Console.WriteLine("Welcome to the bakery. I hope the smell of fresh bread and pastries brought you in this morning!");
+      Start();
+      static void Start()
+      {
+        Console.WriteLine("--------------");
+        Console.WriteLine("Would you like to buy some bread or pastries?  (Bread/Pastry/End)");
+        string response = Console.ReadLine().ToUpper();
+        if (response == "BREAD") 
+        {
+
+        }
+        else if (response == "PASTRY")
+        {
+
+        }
+        else if (response == "END")
+        {
+          End();
+        }
+        else
+        {
+          Console.WriteLine("Sorry, my english isn't perfect. Can you repeat that?");
+          Start();
+        }
+        Console.WriteLine("");
+      }
+      static void End() {};
     }
   }
 }
