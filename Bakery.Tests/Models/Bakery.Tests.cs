5260fbbe57d2e6109c3772dad83135fc8a46e7c6
plymatea <andy.plymate@gmail.com>
2022-02-25 08:41:47 -0800

    set up initial environment.

---
+++ Bakery.Tests/Models/Bakery.Tests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System; 
+using System.Collections.Generic;
+using Bakery.Models;
+
+namespace Bakery.TestTools
+{
+  [TestClass]
+  public class BakeryTests
+  {
+    
+  }
+}
