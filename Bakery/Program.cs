49569ad10c05267b0ec00a1eab630812617c77d0
plymatea <andy.plymate@gmail.com>
2022-02-25 15:48:07 -0800

    refactored bread branching based on number ordered.

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -12,11 +12,11 @@ namespace Bakery
       Console.WriteLine("Good Morning!! Guten Tag!! ");
       Console.WriteLine("Welcome to my bakery. I hope the smell of fresh bread and pastries brought you in this morning!");
       Console.WriteLine("--------------");
-
+      Console.WriteLine("How can I help you today? (Buy/View Cart/End)");
       Start();
       static void Start()
       {
-        Console.WriteLine("How can I help you today? (Buy/View Cart/End)");
+
         string response = Console.ReadLine().ToUpper();
         if (response == "BUY")
         {
@@ -42,12 +42,31 @@ namespace Bakery
         Buy();
         static void Buy()
         {
+          static void BuyStart()
+          {
+            Console.WriteLine("--------------");
+            Console.WriteLine("What else can I do for you? (Buy/View Cart");
+            string response = Console.ReadLine().ToUpper();
+            if (response == "BUY")
+            {
+              Buy();
+            }
+            else if (response == "VIEW CART")
+            {
+              Cart();
+            }
+            else if (response == "END")
+            {
+              End();
+            }
+          }
+
           Console.WriteLine("--------------");
           Console.WriteLine("We are selling bread and pastries this morning.");
-          Console.WriteLine($"The bread is ${Bread.GetPrice()} each or {Bread.GetSaleSet()} loaves for ${Bread.GetSalePricePerSet()}.");
+          Console.WriteLine($"The bread is ${Bread.GetPrice()} each or {Bread.GetSaleSet()} for ${Bread.GetSalePricePerSet()}.");
           Console.WriteLine($"The pastries are ${Pastry.GetPrice()} each or {Pastry.GetSaleSet()} for ${Pastry.GetSalePricePerSet()}.");
           Console.WriteLine("--------------");
-          Console.WriteLine("Would you like some?  (Bread/Pastry)");
+          Console.WriteLine("Which would you like?  (Bread/Pastry/View Cart)");
           string response = Console.ReadLine().ToUpper();
           if (response == "BREAD") 
           {
@@ -62,18 +81,42 @@ namespace Bakery
             }
             if (Int32.Parse(response) <Bread.GetSaleSet())
             {
-            Console.WriteLine($"Bread: ({response}) @ ${Bread.GetPrice()} each >> ${Bread.GetTotalBreadListPrice()}");
-            Console.WriteLine("--------------");
-            Start();
+              Console.WriteLine("Bread:");
+              Console.WriteLine($"   Each: ({response}) @ ${Bread.GetPrice()} >> ${Bread.GetTotalBreadListPrice()}");
+              BuyStart();
+            }
+            else if (Int32.Parse(response) % Bread.GetSaleSet() == 0)
+            {
+              Console.WriteLine("Bread:");
+              int num  = Int32.Parse(response);
+              int saleSets = num / Bread.GetSaleSet();
+              int rem = num % Bread.GetSaleSet();
+              Console.WriteLine($"  Sale Qty:({saleSets}) @ ${Bread.GetSalePricePerSet()} >> ${Bread.GetSalePricePerSet() * saleSets}");
+              BuyStart();
+            }
+            else if (Int32.Parse(response) % Bread.GetSaleSet() != 0)
+            {
+              Console.WriteLine("Bread:");
+              int num  = Int32.Parse(response);
+              int saleSets = num / Bread.GetSaleSet();
+              int rem = num % Bread.GetSaleSet();
+              Console.WriteLine($"  Sale Qty:({saleSets}) @ ${Bread.GetSalePricePerSet()} >> ${Bread.GetSalePricePerSet() * saleSets}");
+              Console.WriteLine($"  Each:({rem}) @ ${Bread.GetPrice()} >> ${(rem) * Bread.GetPrice()}");
+              BuyStart();
             }
             else
             {
-            
+              Console.WriteLine("Sorry, my english isn't perfect. Can you repeat that?");
+              Buy();
             }
           }
           else if (response == "PASTRY")
           {
 
+          }
+          else if (response == "VIEW Cart")
+          {
+            Cart();
           }
           else if (response == "END")
           {
